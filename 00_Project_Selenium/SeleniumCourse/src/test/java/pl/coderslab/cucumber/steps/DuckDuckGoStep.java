b222bc79d36dc0b70eb9a757fc3c25ab93ead494
Rafal Michal Czerwik <rafal.czerwik@gmail.com>
2024-04-07 10:58:37 +0200

    up

--- 00_Project_Selenium/SeleniumCourse/src/test/java/pl/coderslab/cucumber/steps/DuckDuckGoStep.java
+++ 00_Project_Selenium/SeleniumCourse/src/test/java/pl/coderslab/cucumber/steps/DuckDuckGoStep.java
@@ -1,14 +1,46 @@
 package pl.coderslab.cucumber.steps;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
 
 public class DuckDuckGoStep {
 
+    private WebDriver driver;
+
     @Given("an open browser with duckduckgo.com")
     public void openBrowserWithDuckDuckGo() {
-        WebDriver driver = new ChromeDriver();
+
+        this.driver = new ChromeDriver();
         driver.get("https://duckduckgo.com");
+    }
+
+
+    @When("user search for {word}")
+    public void userSearchForKeyWord(String keyword) {
+
+        WebElement element = this.driver.findElement(By.name("q"));
+        element.clear();
+        element.sendKeys(keyword);
+        element.submit();
+    }
+
+    @Then("the first result should contain {word}")
+    public void theFirstResultShouldContainSelenium(String phrase) {
+        WebElement firstResult = this.driver.findElement(By.xpath("//*[@id=\"r1-0\"]/div[1]/div/a"));
+        assertTrue(firstResult.getText().contains(phrase));
+    }
+
+    @And("quit browser")
+    public void quitBrowser() {
+
+        this.driver.quit();
     }
 }
